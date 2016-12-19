32fd36986f036a5abbe95075879eaf26005f1f4c
xufanglong <funnel@163.com>
2016-12-20 00:41:55 +0800

    first time change !

---
+++ chap01/范例1-1/Hello.java
@@ -0,0 +1,22 @@
+
+/**
+
+ * @ClassName: Hello
+
+ * @Description: Hello World 示例小程序
+
+ * @author: YuHong & Kerl
+
+ * @date: 2014年1月16日 上午9:26:09
+
+
+ */
+public class Hello
+{
+    // 是程序的起点，所有程序由此开始运行
+    public static void main(String args[])
+    {
+    	// 此语句表示向屏幕上打印输出"Hello World !"字符串
+    	System.out.println("Hello World !");
+    }
+}
